22e658aee542229721dbd4ccbb6338da7c4b6322
James Cornel <jamescornel7@gmail.com>
2024-03-22 18:47:52 +0800

    added initial page content

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterOutlet,
+    NgbModule,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    NgbModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
